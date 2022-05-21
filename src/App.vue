33f274ef032dd37b49730ab845ef0c05a7267717
a21ns1g4ts <atila.danvi@outlook.com>
2022-05-21 03:30:22 -0300

    Add ibge search

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Concurso IBGE-2022</h1>
+    <p>Resultado final do concurso do IBGE 2022 divulgado pela FGV</p>
+
+    <form @submit.prevent="searchParticipant">
+      <input type="text" v-model="search" placeholder="Pesquisar (nome)" />
+
+      <button type="submit">Pesquisar</button>
+    </form>
+
+    <ul>
+      <strong v-if="results.length">
+        Inscrição Nome Nascimento Jurado Objetiva Final Situação Classificação
+        Cidade
+      </strong>
+      <div v-for="(result, i) in results" :key="i">
+        <p>
+          {{ result.participant }} <strong>{{ result.city }}</strong>
+        </p>
+      </div>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data: function () {
+    return {
+      data: null,
+      search: "",
+      results: [],
+    };
+  },
+
+  methods: {
+    searchParticipant: function () {
+      this.results = this.data.filter(
+        function (item) {
+          return (
+            item.participant.toLowerCase().indexOf(this.search.toLowerCase()) >
+            -1
+          );
+        }.bind(this)
+      );
+    },
+  },
+
+  mounted: function () {
+    fetch("/results.json")
+      .then((res) => res.json())
+      .then((data) => {
+        this.data = data;
+      });
+  },
+};
 </script>
 
 <style>
